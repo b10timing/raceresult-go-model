0f9e22d8098c8f3dd6a450c0df6c2d6f4d697bf9
petersen@raceresult.com <petersen@raceresult.com>
2023-10-11 13:03:18 +0200

    page.Size.ToMM fix

--- page/size.go
+++ page/size.go
@@ -20,29 +20,29 @@ func (s Size) ToMM(landscape bool) (decimal.Decimal, decimal.Decimal) {
 	var w, h decimal.Decimal
 	switch s {
 	case SizeA1:
-		w = 594
-		h = 840
+		w = decimal.FromInt(594)
+		h = decimal.FromInt(840)
 	case SizeA2:
-		w = 420
-		h = 594
+		w = decimal.FromInt(420)
+		h = decimal.FromInt(594)
 	case SizeA3:
-		w = 297
-		h = 420
+		w = decimal.FromInt(297)
+		h = decimal.FromInt(420)
 	case SizeA4:
-		w = 210
-		h = 297
+		w = decimal.FromInt(210)
+		h = decimal.FromInt(297)
 	case SizeA5:
-		w = 148
-		h = 210
+		w = decimal.FromInt(148)
+		h = decimal.FromInt(210)
 	case SizeA6:
-		w = 105
-		h = 148
+		w = decimal.FromInt(105)
+		h = decimal.FromInt(148)
 	case SizeLetter:
-		w = 216 // 85 * 2.54
-		h = 279 // 110 * 2.54
+		w = decimal.FromInt(216) // 85 * 2.54
+		h = decimal.FromInt(279) // 110 * 2.54
 	case SizeLegal:
-		w = 216 // 85 * 2.54
-		h = 356 // 140 * 2.54
+		w = decimal.FromInt(216) // 85 * 2.54
+		h = decimal.FromInt(356) // 140 * 2.54
 	}
 
 	if landscape {
