ca6133fba124f5707eace3455340cd5b074e9c2c
petersen@raceresult.com <petersen@raceresult.com>
2025-03-26 11:38:34 +0100

    PreviewIDList removed

--- emailtemplate/preview.go
+++ emailtemplate/preview.go
@@ -1,28 +1,5 @@
 package emailtemplate
 
-import (
-	"strconv"
-	"strings"
-
-	"github.com/raceresult/go-model/variant"
-)
-
-type PreviewIDList []int
-
-func (q *PreviewIDList) UnmarshalJSON(data []byte) error {
-	s := string(data)
-	s = strings.TrimPrefix(s, "\"")
-	s = strings.TrimSuffix(s, "\"")
-	*q = PreviewIDList(variant.StringList(strings.Split(s, ",")).ToInt())
-	return nil
-}
-func (q *PreviewIDList) MarshalJSON() ([]byte, error) {
-	if len(*q) == 1 {
-		return []byte(strconv.Itoa((*q)[0])), nil
-	}
-	return []byte("\"" + strings.Join(variant.IntList(*q).ToString(), ",") + "\""), nil
-}
-
 type Preview struct {
 	Type        Type
 	Bibs        []int
